ded77d9f250df7879f0c809990ae5076da005000
Danny Finnegan <57728938+finferno00@users.noreply.github.com>
2022-01-24 15:06:15 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
-let ring = neopixel.create(DigitalPin.P0, 24, NeoPixelMode.RGB)
+let ring = neopixel.create(DigitalPin.P0, 15, NeoPixelMode.RGB_RGB)
 basic.forever(function () {
     for (let index = 0; index <= 23; index++) {
-        ring.setPixelColor(index, neopixel.colors(NeoPixelColors.Blue))
+        ring.setPixelColor(index, neopixel.colors(NeoPixelColors.Yellow))
         ring.show()
         basic.pause(50)
         ring.setPixelColor(index, neopixel.rgb(16, 16, 16))
